da2b86785d0a41bd2caa581cf6c31a2f413c381f
2298814528 <2298814528@qq.com>
2020-07-09 19:35:42 +0800

    first commit

---
+++ src/test/java/com/zjj/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjj.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
