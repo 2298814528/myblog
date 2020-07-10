df0c7c6884ce1776e6d916744f09a053c2b45db5
2298814528 <2298814528@qq.com>
2020-07-11 00:58:23 +0800

    增加前端页面  by zjj 2020/7/11 0:58

---
+++ src/main/java/com/zjj/myblog/entity/UserCollection.java
@@ -0,0 +1,30 @@
+package com.zjj.myblog.entity;
+
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author author
+ * @since 2020-07-11
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class UserCollection implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long userId;
+
+    private Long postId;
+
+    private Long postUserId;
+
+
+}
---
+++ src/main/java/com/zjj/myblog/mapper/UserMessageMapper.java
@@ -0,0 +1,16 @@
+package com.zjj.myblog.mapper;
+
+import com.zjj.myblog.entity.UserMessage;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author author
+ * @since 2020-07-11
+ */
+public interface UserMessageMapper extends BaseMapper<UserMessage> {
+
+}
