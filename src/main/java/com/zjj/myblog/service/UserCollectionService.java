5d587b7f9d334b11846dacd71ac29ad9b14d716f
2298814528 <2298814528@qq.com>
2020-07-11 01:16:47 +0800

    代码生成  by zjj 2020/7/11 1:16

---
+++ src/main/java/com/zjj/myblog/service/UserCollectionService.java
@@ -0,0 +1,16 @@
+package com.zjj.myblog.service;
+
+import com.zjj.myblog.entity.UserCollection;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author author
+ * @since 2020-07-11
+ */
+public interface UserCollectionService extends IService<UserCollection> {
+
+}
