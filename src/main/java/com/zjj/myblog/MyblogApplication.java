4b8970ec3ad27cfc7c623ea5808096fd77dca5fe
2298814528 <2298814528@qq.com>
2020-07-10 14:42:02 +0800

    by zjj 10111

--- src/main/java/com/zjj/myblog/MyblogApplication.java
+++ src/main/java/com/zjj/myblog/MyblogApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyblogApplication {
 
     public static void main(String[] args) {
+        System.out.println("http://localhost:8080/blog");
         SpringApplication.run(MyblogApplication.class, args);
     }
 
